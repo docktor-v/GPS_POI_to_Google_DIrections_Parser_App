ee980b6c3590c3e3a5df23143cd6aae5bb490325
docktor-v <68287357+docktor-v@users.noreply.github.com>
2021-12-17 12:35:52 -0500

    Commit after refactor 2

--- src/main/java/org/personal/UserInterface.java
+++ src/main/java/org/personal/UserInterface.java
@@ -6,15 +6,22 @@
 package org.personal;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.nio.file.DirectoryStream;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -23,6 +30,8 @@ import java.util.Scanner;
  */
 public class UserInterface extends Application {
 
+    private final ObservableList<POI> data
+            = FXCollections.observableArrayList();
 
     @Override
     public void start(Stage stage) {
@@ -30,26 +39,108 @@ public class UserInterface extends Application {
         POIFactory poiFactory = new POIFactory();
         HashMap<String, POI> POIMap = new HashMap<>();
         POIs POIList = new POIs();
+        TableView table;
 
-        Table table = new Table(POIList);
         VBox tableVBox = new VBox();
         Scene scene = new Scene(new Group());
-//5///aaaa
-//        BorderPane borderPane = new BorderPane();
-//        borderPane.setPrefSize(600, 600);
 
         stage.setTitle(
                 "POI Parser to Google Map Directions");
-        tableVBox = table.getVBox(stage);
-        HBox hbox = new HBox();
-        Button btn = new Button("button");
-        //tableVBox.getChildren()
 
 
+        ChoiceBox<String> choiceBox = new ChoiceBox();
+        choiceBox.getItems().addAll("Latitude", "Longitude", "Substation Name", "City");
+        choiceBox.setValue("Substation Name");
+//        FileChooser fileChooser = new FileChooser();
+//
+//        Button button = new Button("Select File");
+//        button.setOnAction(e -> {
+//            File selectedFile = fileChooser.showOpenDialog(stage);
+//        });
+        POIList = poiFactory.createPOIs("Duke Directions Spreadsheet.csv");
+        POIMap = POIList.getPOIs();
+        initData(POIMap);
+        FilteredList<POI> flPOI = new FilteredList(data, p -> true);//Pass the data to a filtered list
+        Table tableConst = new Table(POIList);
 
-        ((Group) scene.getRoot()).getChildren().addAll(tableVBox);
+
+        table = tableConst.getTable(stage);
+
+        final Label label = new Label("POI Data");
+        label.setFont(new Font("Arial", 20));
+
+        table = createCollumns(table, flPOI);
+
+        TextField textField = new TextField();
+        textField.setPrefWidth(450);
+        textField.setPromptText("Choose field from drop down and search here");
+        textField.textProperty().addListener((obs, oldValue, newValue) -> {
+            switch (choiceBox.getValue())//Switch on choiceBox value
+            {
+                case "Lattitude":
+                    flPOI.setPredicate(p -> p.getLatitude().toLowerCase().contains(newValue.toLowerCase().trim()));//filter table by first name
+                    break;
+                case "Longitude":
+                    flPOI.setPredicate(p -> p.getLongitude().toLowerCase().contains(newValue.toLowerCase().trim()));//filter table by last name
+                    break;
+                case "Substation Name":
+                    flPOI.setPredicate(p -> p.getSubName().toLowerCase().contains(newValue.toLowerCase().trim()));//filter table by email
+                    break;
+                case "City":
+                    flPOI.setPredicate(p -> p.getSubName().toLowerCase().contains(newValue.toLowerCase().trim()));//filter table by email
+                    break;
+            }
+        });
+
+        choiceBox.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal)
+                -> {//reset table and textfield when new choice is selected
+            if (newVal != null) {
+                textField.setText("");
+            }
+        });
+
+        HBox hBox = new HBox(choiceBox, textField);//Add choiceBox and textField to hBox
+        hBox.setAlignment(Pos.CENTER);//Center HBox
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(label, table, hBox);
+
+        ((Group) scene.getRoot()).getChildren().addAll(vbox);
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void initData(HashMap<String, POI> POIList) {
+
+        POIList.values().stream().forEach(poi -> data.add(poi));
+    }
+
+    private TableView createCollumns(TableView table, FilteredList flPOI) {
+        table.setItems(flPOI);//Set the table's items using the filtered list
+
+        TableColumn substationCol = new TableColumn("Substation Name");
+        substationCol.setMinWidth(200);
+        substationCol.setCellValueFactory(
+                new PropertyValueFactory<POI, String>("subName"));
+
+        TableColumn latitudeCol = new TableColumn("Latitude");
+        latitudeCol.setMinWidth(100);
+        latitudeCol.setCellValueFactory(
+                new PropertyValueFactory<POI, String>("latitude"));
+
+        TableColumn longitudeCol = new TableColumn("Longitude");
+        longitudeCol.setMinWidth(100);
+        longitudeCol.setCellValueFactory(
+                new PropertyValueFactory<POI, String>("longitude"));
+
+        TableColumn cityCol = new TableColumn("City");
+        cityCol.setMinWidth(100);
+        cityCol.setCellValueFactory(
+                new PropertyValueFactory<POI, String>("city"));
+
+        table.getColumns().addAll(substationCol, latitudeCol, longitudeCol, cityCol);
+        return table;
     }
 
     public static void main(String[] args) {
